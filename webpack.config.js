416129ce39c9b2c6e958d9163ef3e8b96f97626c
Kalinss <Kalinss16@gmail.com>
2020-08-24 01:14:59 +0400

    update config added htmlWebpackPlugin, env variables, build and dev scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isProduction = NODE_ENV === "production" || false;
+const isDevelopment = NODE_ENV === "development" || false;
 
 module.exports = {
   entry: "./src/index.ts",
-  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][hash].js",
   },
+
+  devtool: isDevelopment && "inline-source-map",
+  stats: isDevelopment ? "minimal" : "normal",
+
   module: {
     rules: [
       {
@@ -19,4 +28,15 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
